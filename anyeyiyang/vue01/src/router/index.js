ce702c2e9e70937e9dbdc053694eb86e28335a39
dfgdre <1286239506@qq.com>
2019-07-31 17:19:31 +0800

    111

--- anyeyiyang/vue01/src/router/index.js
+++ anyeyiyang/vue01/src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import wei from "../components/Wei_c"
+import qyh from "../components/changshi_qyh"
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {path:'/a',component:wei}
+    {path:'/a',component:wei},
+    {path:'/qyh',component:qyh}
   ]
 })
